87622d887f942e069ea55c4865a1ef6ac6020384
Hiếu Lê <hieu.le@zen8labs.com>
2024-12-08 19:48:31 +0700

    initial commit

---
+++ src/test/java/emulator/banking/YianApplicationTests.java
@@ -0,0 +1,13 @@
+package emulator.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
